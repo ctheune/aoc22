4a7dcd14a56f7434fe2c5570251f52497688d403
Christian Theune <ct@flyingcircus.io>
2022-12-04 08:35:53 +0100

    day 4

---
+++ day4/solution2.py
@@ -0,0 +1,15 @@
+reconsider = 0
+
+
+def expand_range(r):
+    left, right = map(int, r.split("-"))
+    return set(range(left, right + 1))
+
+
+for pair in open("input.txt"):
+    first, second = map(expand_range, pair.strip().split(","))
+    if first.intersection(second):
+        reconsider += 1
+
+
+print(reconsider)
